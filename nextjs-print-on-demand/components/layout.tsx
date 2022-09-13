2312a888669979017145a88a6e7206a39914d013
willp11 <williampage11@yahoo.co.uk>
2022-09-13 18:26:49 +0700

    fixed layout again

--- nextjs-print-on-demand/components/layout.tsx
+++ nextjs-print-on-demand/components/layout.tsx
@@ -4,8 +4,8 @@ import Footer from "./footer";
 
 export default function Layout({children}: {children: ReactNode}) {
     return (
-        <div className="w-full bg-white">
-            <div className="w-full max-w-[1200px] min-h-[100vh] mx-auto">
+        <div className="w-full bg-white flex flex-col justify-between min-h-[100vh]">
+            <div className="w-full max-w-[1200px] mx-auto">
                 <div className="flex flex-col justify-start">
                     <Header />
                     {children}
