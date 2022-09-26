b429e92d90cc05dc4e1a95c3dcc6c0ddb868d3c6
willp11 <williampage11@yahoo.co.uk>
2022-09-26 17:24:53 +0700

    installed auth packages

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
