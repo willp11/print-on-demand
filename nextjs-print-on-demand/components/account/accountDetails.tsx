3977f6a0757ef6a4ca522047981b24311531489f
willp11 <williampage11@yahoo.co.uk>
2022-09-27 16:38:57 +0700

    created account details page

---
+++ nextjs-print-on-demand/components/account/accountDetails.tsx
@@ -0,0 +1,60 @@
+import { PencilSquareIcon, CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
+const DetailsField = ({title, content, placeholder}: {title: string, content: string, placeholder: string}) => {
+
+    const [editMode, setEditMode] = useState(false);
+    const toggleEditMode = () => {
+        setEditMode(!editMode);
+    }
+
+    if (!editMode) {
+        return (
+            <div className="mb-2">
+                <h2 className="text-sm text-gray-500 font-semibold">{title}</h2>
+                <div className="flex items-center">
+                    <p>{content}</p>
+                    <PencilSquareIcon className="w-4 h-4 cursor-pointer ml-2 hover:stroke-blue-600" onClick={toggleEditMode} />
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="mb-2">
+                <h2 className="text-sm text-gray-500 font-semibold">{title}</h2>
+                <div className="flex items-center">
+                    <input type="text" placeholder={placeholder} className="p-1 border border-gray-300" />
+                    <XCircleIcon className="w-6 h-6 cursor-pointer ml-2 stroke-red-600 hover:stroke-red-700" onClick={toggleEditMode} />
+                    <CheckCircleIcon className="w-6 h-6 cursor-pointer ml-2 stroke-green-600 hover:stroke-green-700" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default function AccountDetails() {
+    return (
+        <div className="w-full p-2">
+            <div className="mb-2">
+                <h2 className="text-sm text-gray-500 font-semibold">Email</h2>
+                <div className="flex items-center">
+                    <p>test@test.com</p>
+                </div>
+            </div>
+            <DetailsField title="First Name" content="William" placeholder="Type new name..." />
+            <DetailsField title="Last Name" content="Page" placeholder="Type new name..." />
+            <DetailsField title="Phone Number" content="01234567890" placeholder="Type new number..." />
+            <div className="mb-2">
+                <h2 className="text-sm text-gray-500 font-semibold">Change Password</h2>
+                <button className="btn mt-1" style={{width: "120px"}}>CHANGE</button>
+            </div>
+            <div className="mb-2">
+                <h2 className="text-sm text-gray-500 font-semibold">Marketing Emails</h2>
+                <div className="flex items-center">
+                    <input type="checkbox" className="cursor-pointer" checked={true} />
+                    <p className="text-sm ml-2">You are opted-in to receiving marketing emails.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- nextjs-print-on-demand/pages/account/index.tsx
+++ nextjs-print-on-demand/pages/account/index.tsx
@@ -1,9 +1,11 @@
-import AccountMenu from "../../components/account/accountMenu"
+import AccountMenu from "../../components/account/accountMenu";
+import AccountDetails from "../../components/account/accountDetails";
 
 export default function Account() {
     return (
         <div className="h-[500px] w-full flex justify-start border border-gray-300 rounded-md shadow-md my-4">
             <AccountMenu />
+            <AccountDetails />
         </div>
     )
 }
