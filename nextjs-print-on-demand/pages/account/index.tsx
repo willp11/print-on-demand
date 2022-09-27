bc1c96ccd20088763eda881701f4235b0c9187f8
willp11 <williampage11@yahoo.co.uk>
2022-09-27 17:39:00 +0700

    added wishList

--- nextjs-print-on-demand/pages/account/index.tsx
+++ nextjs-print-on-demand/pages/account/index.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import AccountMenu from "../../components/account/accountMenu";
 import AccountDetails from "../../components/account/accountDetails";
+import WishList from "../../components/account/wishList";
 
 export default function Account() {
 
     const [selectedIndex, setSelectedIndex] = useState(0);
 
     return (
-        <div className="h-[500px] w-full flex justify-start border border-gray-300 rounded-md shadow-md my-4">
+        <div className="h-[500px] w-full flex justify-start border border-gray-300 rounded-md shadow-md my-4 overflow-y-auto">
             <AccountMenu selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
             {(selectedIndex === 0) && <AccountDetails />}
+            {(selectedIndex === 2) && <WishList />}
         </div>
     )
 }
