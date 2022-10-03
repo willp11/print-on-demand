bc7439ff76c9f9b9f97192084bbf0cde1c9cff0d
willp11 <williampage11@yahoo.co.uk>
2022-10-03 17:54:11 +0700

    implemented retrieve product endpoint

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('get-product-list/', ProductListView.as_view(), name='get_product_list')
+    path('get-product-list/', ProductListView.as_view(), name='get_product_list'),
+    path('get-product/<int:pk>/', ProductRetrieveView.as_view(), name='get_product')
 ]
--- backend/shop/views.py
+++ backend/shop/views.py
@@ -1,5 +1,6 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
+from django.shortcuts import get_object_or_404
 from .serializers import *
 from .models import *
 
@@ -7,3 +8,11 @@ class ProductListView(ListAPIView):
     queryset = Product.objects.all()
     permission_classes = [AllowAny]
     serializer_class = ProductSerializer
+
+class ProductRetrieveView(RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = ProductSerializer
+
+    def get_object(self):
+        product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        return product
