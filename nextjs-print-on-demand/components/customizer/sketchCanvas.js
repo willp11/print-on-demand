14384ecefaa88313d1e5884dc0de5726bd40b281
willp11 <williampage11@yahoo.co.uk>
2022-10-20 17:16:11 +0700

    fixed runtime bug in sketch

--- nextjs-print-on-demand/components/customizer/sketchCanvas.js
+++ nextjs-print-on-demand/components/customizer/sketchCanvas.js
@@ -222,10 +222,10 @@ export default function SketchCanvas() {
         }
 
         // PRINTABLE AREA
-        let printable_x_start = product.drawableArea[productSide].xPos * (canvasSize/500);
-        let printable_y_start = product.drawableArea[productSide].yPos * (canvasSize/500);
-        let printable_size_x = product.drawableArea[productSide].xSize * (canvasSize/500);
-        let printable_size_y = product.drawableArea[productSide].ySize * (canvasSize/500);
+        let printable_x_start = product?.drawableArea[productSide].xPos * (canvasSize/500);
+        let printable_y_start = product?.drawableArea[productSide].yPos * (canvasSize/500);
+        let printable_size_x = product?.drawableArea[productSide].xSize * (canvasSize/500);
+        let printable_size_y = product?.drawableArea[productSide].ySize * (canvasSize/500);
 
         if (selectedLayer !== null && selectedLayer !== undefined && activeLayerRef.current !== undefined) {
             // printable rectangle
